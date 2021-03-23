a8be579455f9033c5ef165b9233d69bb4e462a1b
sergiolysenko <54682953+sergiolysenko@users.noreply.github.com>
2021-03-23 16:41:06 +0300

    create pictures model

---
+++ src/model/picture.js
@@ -0,0 +1,44 @@
+
+import {Observer} from "../utils/observer.js";
+
+export class PictureModel extends Observer {
+  constructor() {
+    super();
+    this._pictures = [];
+  }
+
+  setPictures(updateType, pictures) {
+    this._pictures = pictures.slice();
+
+    this._notify(updateType);
+  }
+
+  getPictures() {
+    return this._pictures;
+  }
+
+  updatePicture(updaeType, update) {
+    const index = this._pictures.findIndex((picture) => picture.id === update.id);
+
+    if (index === -1) {
+      throw new Error("Can't update unexisting picture")
+    }
+
+    this._pictures = [
+      ...this._pictures.slice(0, index),
+      update,
+      ...this._pictures.slice(index + 1)
+    ]
+
+    this._notify(updateType, update);
+  }
+
+  addPicture(updateType, update) {
+    this._pictures = [
+      update,
+      ...this._pictures
+    ];
+
+    this._notify(updateType, update);
+  }
+}
