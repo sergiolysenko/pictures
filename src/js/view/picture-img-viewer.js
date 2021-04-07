76c042eda426b5c199bf800c6390210776706aa0
sergiolysenko <54682953+sergiolysenko@users.noreply.github.com>
2021-04-07 18:05:44 +0300

    fix picutre-viewer img width and height

--- src/js/view/picture-img-viewer.js
+++ src/js/view/picture-img-viewer.js
@@ -4,8 +4,8 @@ const createImgViewerTemplate = (picture) => {
   const {src} = picture;
 
   return `<div class="picture-viewer">
-    <div class="fixed z-50 w-screen">
-      <img src=${src} class="object-cover h-screen mx-auto">
+    <div class="fixed h-screen w-screen flex z-50 align-center justify-center">
+      <img src=${src} class="object-contain w-screen">
     </div>
     <div class="fixed inset-0 bg-gray-500 bg-opacity-75 z-40"></div>
   </div>`
