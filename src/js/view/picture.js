e6240f78e2bb0d5a7e02dde6b84207d10d364e9c
sergiolysenko <54682953+sergiolysenko@users.noreply.github.com>
2021-04-07 18:34:51 +0300

    test bug fix for ios safari

--- src/js/view/picture.js
+++ src/js/view/picture.js
@@ -11,6 +11,9 @@ const createPictureTemplate = (picture) => {
           <polygon fill="white" points="0,303.947 0,383.947 80,383.947 316.053,147.893 236.053,67.893"></polygon>
           <path fill="white" d="M377.707,56.053L327.893,6.24c-8.32-8.32-21.867-8.32-30.187,0l-39.04,39.04l80,80l39.04-39.04 C386.027,77.92,386.027,64.373,377.707,56.053z"></path>
         </svg>
+        <svg fill="white" class="height="20" width="20" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 130">
+          <path stroke="white" d="M87.5 111l-47-47m0 0l47-47" stroke-width="15"></path>
+        </svg>
       </button>
       <div class="picture-view-box w-full h-80 relative">
         <img src="${src}" alt="${title}" class="object-cover w-full h-full bg-gray-100 rounded-t-lg" />
